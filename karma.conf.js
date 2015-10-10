3ccb3a087e1cc96e3fe029a6b43a74b589a5503e
alphatools <sergej.tissen@gmail.com>
2015-10-10 16:02:18 +0000

    unit tests moved

--- karma.conf.js
+++ karma.conf.js
@@ -20,8 +20,7 @@ function listFiles() {
     .concat([
       path.join(conf.paths.src, '/app/**/*.module.js'),
       path.join(conf.paths.src, '/app/**/*.js'),
-      path.join(conf.paths.src, '/**/*.spec.js'),
-      path.join(conf.paths.src, '/**/*.mock.js'),
+      path.join(conf.paths.unit, '/**/*.js'),
     ])
     .concat(pathSrcHtml);
 }
