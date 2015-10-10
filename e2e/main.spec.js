17124374968f263f0e96dc63bfc269042afab13b
alphatools <sergej.tissen@gmail.com>
2015-10-10 19:36:36 +0000

    e2e bug test

--- e2e/main.spec.js
+++ e2e/main.spec.js
@@ -9,7 +9,7 @@ describe('The main view', function () {
   });
 
   it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allos!');
+    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
     expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
     expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
   });
